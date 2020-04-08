1ebc0a59303a0fe498d6f99f85417ee68794a5ff
SpiralP <spiral.palmer@gmail.com>
2020-04-07 22:23:05 -0700

    init

---
+++ build.rs
@@ -0,0 +1,55 @@
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+};
+
+fn main() {
+    let target = env::var("TARGET").unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    let mut nmake = cc::windows_registry::find(&target, "nmake.exe").unwrap();
+
+    assert!(nmake.current_dir("Detours/src").status().unwrap().success());
+
+    fs::copy(
+        "Detours/lib.X64/detours.lib",
+        Path::new(&out_dir).join("detours.lib"),
+    )
+    .unwrap();
+
+    // Tell cargo to tell rustc to link the system bzip2
+    // shared library.
+    println!("cargo:rustc-link-lib=static=detours");
+
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=wrapper.h");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        .clang_arg("-IDetours/include")
+        // The input header we would like to generate
+        // bindings for.
+        .header_contents(
+            "bindgen.h",
+            r#"
+                #include <Windows.h>
+                #include "detours.h"
+            "#
+        )
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .whitelist_function("Detour.*")
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
